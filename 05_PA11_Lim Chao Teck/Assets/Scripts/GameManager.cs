3939641cc8ec981f0a05286c8a125cb664082738
Whitebeard581 <64114893+Whitebeard581@users.noreply.github.com>
2020-06-18 16:04:31 +0800

    Updated by Chao Teck

---
+++ 05_PA11_Lim Chao Teck/Assets/Scripts/GameManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager thisManager;
+
+    private int score = 0;
+    private int lives = 1;
+    private int highScore = 0;
+
+    public Text Txt_Score;
+    public Text Txt_Highscores;
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisManager = this;
+
+        highScore = PlayerPrefs.GetInt("HighScore");
+        Txt_Highscores.text = "Highscore : " + highScore;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateScore()
+    {
+        score++;
+        Txt_Score.text = "Score : " + score;
+    }
+
+    public void GameOver()
+    {
+        if (score > highScore)
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+        }
+        SceneManager.LoadScene("GameLose");
+    }
+
+    public void UpdateLives()
+    {
+        lives--;
+
+        if (lives <= 0)
+        {
+            GameOver();
+        }
+    }
+}
---
+++ 05_PA11_Lim Chao Teck/Assets/Scripts/ObstacleMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleMovement : MonoBehaviour
+{
+    private float xSpeed = -4f;
+
+    private bool Flag = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(xSpeed*Time.deltaTime, 0, 0f));
+        if (transform.position.x < -8f)
+        {
+            Destroy(this.gameObject);
+        }
+
+        if (transform.position.x <= -7.5 && Flag == false)
+        {
+            Flag = true;
+            GameManager.thisManager.UpdateScore();
+        }
+    }
+}
